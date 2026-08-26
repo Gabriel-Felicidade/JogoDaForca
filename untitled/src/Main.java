b1f29ac21c45e9031563114d9d217851529ad382
Gabriel-Felicidade <mandeumemailparagabriel@gmail.com>
2026-08-26 02:13:42 -0300

    feat: define palavra secreta, mascara de progresso e busca de caractere

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello, World");
+        String palavraSecreta = "MENSAGEM";
+
+        char[] letrasDescobertas = new char [palavraSecreta.length()];
+        for (int i = 0; i < letrasDescobertas.length; i++) {
+            letrasDescobertas[i] = '_';
+        }
+
+        char chute = 'M';
+
+        for (int i = 0; i < palavraSecreta.length(); i++) {
+            if (palavraSecreta.charAt(i) == chute) {
+                letrasDescobertas[i] = chute;
+            }
+        }
+
+        System.out.println(String.valueOf(letrasDescobertas));
     }
 }
+
