d1269e80b1fe18f4773d3d3ac700403e4c77a0e7
Gabriel-Felicidade <mandeumemailparagabriel@gmail.com>
2026-09-02 01:59:00 -0300

    feat: implementa loop principal com condicoes de vitoria e derrota

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         String palavraSecreta = "MENSAGEM";
 
         int vidas = 6;
@@ -13,31 +13,47 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Palavra: " + String.valueOf(letrasDescobertas));
-        System.out.println("Vidas restantes: " + vidas);
+        while (vidas > 0 && !acertouTodasAsLetras(letrasDescobertas)) {
+            System.out.println("\n----------------------------------");
+            System.out.println("Palavra: " + String.valueOf(letrasDescobertas));
+            System.out.println("Vidas reestantes: " + vidas);
+            System.out.print("Digite uma letra: ");
 
-        System.out.print("Digite uma letras: ");
-        char chute = scanner.next().toUpperCase().charAt(0);
+            char chute = scanner.next().toUpperCase().charAt(0);
+
+            boolean acertouLetra = false;
+
+            for (int i = 0; i < palavraSecreta.length(); i++) {
+                if (palavraSecreta.charAt(i) == chute) {
+                    letrasDescobertas[i] = chute;
+                    acertouLetra = true;
+                }
+            }
+
+            if (!acertouLetra) {
+                vidas--;
+                System.out.println("\nQue pena! A letra '" + chute + "' nao esta na palavra.");
+            } else {
+                System.out.println("\nBoa! Voce acertou a letra '" + chute + "' .");
+            }
+
+        }
         scanner.close();
 
-        boolean acertouLetra = false;
-
-        for (int i = 0; i < palavraSecreta.length(); i++) {
-            if (palavraSecreta.charAt(i) == chute) {
-                letrasDescobertas[i] = chute;
-                acertouLetra = true;
+        System.out.println("\n==================================");
+        if (acertouTodasAsLetras(letrasDescobertas)) {
+            System.out.println("PARABENS! Voce venceu! A palvra era: " + palavraSecreta);
+        } else {
+            System.out.println("FIM DE JOGO! Suas vidas acabaram. A palavra era: " + palavraSecreta);
+        }
+            System.out.println("==================================");
+    }
+    private static boolean acertouTodasAsLetras(char[] letras) {
+        for (int i = 0; i < letras.length; i++) {
+            if (letras[i] == '_') {
+                return false;
             }
         }
-
-        if (!acertouLetra) {
-            vidas--;
-            System.out.println("\nQue pena! A Letra '" + chute + "' nao esta na palavra.");
-        } else {
-            System.out.println("\nBoa! Voce acertou a letra '" + chute + "'.");
-        }
-
-        System.out.println("Palavra: " + String.valueOf(letrasDescobertas));
-        System.out.println("VIdas restantes: " + vidas);
+        return true;
     }
 }
-
