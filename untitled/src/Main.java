d13111b926db693c7782dc0f0a1fc92b559b4fd5
Gabriel-Felicidade <mandeumemailparagabriel@gmail.com>
2026-09-03 01:01:36 -0300

    feat: adiciona desenho ascii da forca, historico de chutes e validacao de caracteres

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String palavraSecreta = "MENSAGEM";
+        String palavraSecreta = "FELICIDADE";
 
         int vidas = 6;
 
@@ -11,18 +11,34 @@ public class Main {
             letrasDescobertas[i] = '_';
         }
 
+        String letrasTentadas = "";
+
         Scanner scanner = new Scanner(System.in);
 
         while (vidas > 0 && !acertouTodasAsLetras(letrasDescobertas)) {
             System.out.println("\n----------------------------------");
-            System.out.println("Palavra: " + String.valueOf(letrasDescobertas));
+            desenharForca(vidas);
+            imprimirProgressoFormatado(letrasDescobertas);
+            System.out.println("Letras tentadas: " + (letrasTentadas.isEmpty() ? "Nenhuma" : letrasTentadas));
             System.out.println("Vidas reestantes: " + vidas);
             System.out.print("Digite uma letra: ");
 
-            char chute = scanner.next().toUpperCase().charAt(0);
+            String entrada = scanner.next().toUpperCase();
+            char chute = entrada.charAt(0);
+
+            if (!Character.isLetter(chute)) {
+                System.out.println("\nEntrada invalida! Digite apenas letras do alfabeto.");
+                continue;
+            }
+
+            if (letrasTentadas.contains(String.valueOf(chute))) {
+                System.out.println("\nVoce ja tentou a letra '" + chute + "'. Tente outra!");
+                continue;
+            }
+
+            letrasTentadas += chute + " ";
 
             boolean acertouLetra = false;
-
             for (int i = 0; i < palavraSecreta.length(); i++) {
                 if (palavraSecreta.charAt(i) == chute) {
                     letrasDescobertas[i] = chute;
@@ -31,23 +47,26 @@ public class Main {
             }
 
             if (!acertouLetra) {
-                vidas--;
+                vidas --;
                 System.out.println("\nQue pena! A letra '" + chute + "' nao esta na palavra.");
             } else {
-                System.out.println("\nBoa! Voce acertou a letra '" + chute + "' .");
+                System.out.println("\nBoa! Voce acertou a letra '" + chute + "'.");
             }
-
         }
+
         scanner.close();
 
         System.out.println("\n==================================");
         if (acertouTodasAsLetras(letrasDescobertas)) {
+            imprimirProgressoFormatado(letrasDescobertas);
             System.out.println("PARABENS! Voce venceu! A palvra era: " + palavraSecreta);
         } else {
+            desenharForca(vidas);
             System.out.println("FIM DE JOGO! Suas vidas acabaram. A palavra era: " + palavraSecreta);
         }
             System.out.println("==================================");
     }
+
     private static boolean acertouTodasAsLetras(char[] letras) {
         for (int i = 0; i < letras.length; i++) {
             if (letras[i] == '_') {
@@ -56,4 +75,67 @@ public class Main {
         }
         return true;
     }
+
+    private static void imprimirProgressoFormatado(char[] letras) {
+        System.out.print("Palavra: ");
+        for (int i = 0; i < letras.length; i++) {
+            System.out.print(letras[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void desenharForca(int vidas){
+        System.out.println("  +---+");
+        System.out.println("  |   |");
+
+        switch (vidas) {
+            case 6:
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("      |");
+                break;
+            case 5:
+                System.out.println("  O   |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("      |");
+                break;
+            case 4:
+                System.out.println("  O   |");
+                System.out.println("  |   |");
+                System.out.println("      |");
+                System.out.println("      |");
+                break;
+            case 3:
+                System.out.println("  O   |");
+                System.out.println(" /|   |");
+                System.out.println("      |");
+                System.out.println("      |");
+                break;
+            case 2:
+                System.out.println("  O   |");
+                System.out.println(" /|\\  |");
+                System.out.println("      |");
+                System.out.println("      |");
+                break;
+            case 1:
+                System.out.println("  O   |");
+                System.out.println(" /|\\  |");
+                System.out.println(" /    |");
+                System.out.println("      |");
+                break;
+            case 0:
+            default:
+                System.out.println("  O   |");
+                System.out.println(" /|\\  |");
+                System.out.println(" / \\  |");
+                System.out.println("      |");
+                break;
+        }
+        System.out.println("=========");
+    }
 }
+
+
+
